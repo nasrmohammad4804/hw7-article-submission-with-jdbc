1625e4827c208a10e1efcc5263130296b8ef4d75
mohammad nasr <nasrmohammad4804@gmail.com>
2021-07-19 00:30:13 +0430

    writing menu contains login and register and showAllArticle witch publish and article publish and free and article publish and none free and block or unblock account user

--- Hw7/src/service/App.java
+++ Hw7/src/service/App.java
@@ -1,5 +1,6 @@
 package service;
 
+import domain.User;
 import repository.*;
 
 import java.sql.Connection;
@@ -53,7 +54,85 @@ public class App {
 
     }
 
-    public void menu(){
+    public void menu() throws SQLException {
+        System.out.println("enter 1 to login ");
+        System.out.println("enter 2 to register");
+        System.out.println("enter 3 to show article witch published without register or login");
+        System.out.println("enter 4 to show article witch published and free");
+        System.out.println("enter 5 to show article witch published and dont free");
+        System.out.println("enter 6 to block account of user");
+        System.out.println("enter 7 to unBlock account of user ... ");
+        System.out.println("enter 8 to charge of account for user !!!");
+        System.out.println("enter 9 to exit the program");
+
+        int number=scanner.nextInt();
+        scanner.nextLine();
+        switch (number){
+            case 1 : login();
+                     break;
+
+            case 2 : register();
+                     break;
+
+            case 3 : articleRepository.showAll(connection,null);
+                     menu();
+
+            case 4 :  articleRepository.showAllFreeArticle(connection);
+                      menu();
+
+            case 5 : articleRepository.showAllNonFreeArticle(connection);
+
+            case 6 : blockOfUser();
+                     menu();
+
+            case 7 : unBlockOfUser();
+                     menu();
+
+            case 8 : chargeAccountOfUser();
+                    menu();
+
+            case 9 :
+                System.out.println("have nice day by ...!!");
+                System.exit(0);
+
+
+        }
+
+    }
+    public void chargeAccountOfUser() throws SQLException {
+
+        System.out.println("enter your userName ...");
+        String userName =scanner.nextLine();
+        User user = userTable.checkExistsUser(userName,connection) ;
+        if(userTable.hasUserWitchUnBlockAccount(user,connection) ){
+            System.out.println("enter how many balance to add your account ...");
+            int balance=scanner.nextInt();
+            accountRepository.chargeAccount(user,balance,connection);
+        }
+        else System.out.println("this user not exists or blocked accounted -_-");
+    }
+
+    public void blockOfUser() throws SQLException {
+        System.out.println("enter userName");
+        String userName = scanner.nextLine();
+
+      User user = userTable.checkExistsUser(userName,connection);
+
+            UserAdmin.blockAccount(user,connection);
+      }
+
+      public void unBlockOfUser()throws SQLException{
+          System.out.println("enter userName");
+          String userName =scanner.nextLine();
+
+          User user =userTable.checkExistsUser(userName,connection);
+          UserAdmin.unBlockAccount(user,connection);
+      }
+
+    public void login(){
+
+    }
+    public void register(){
 
     }
 }
