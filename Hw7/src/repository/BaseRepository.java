90a112e5814891441c7b959985cf096621f3796f
mohammad nasr <nasrmohammad4804@gmail.com>
2021-07-18 16:51:12 +0430

    merge into services

---
+++ Hw7/src/repository/BaseRepository.java
@@ -0,0 +1,24 @@
+package repository;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface BaseRepository {
+
+
+    void showAll(Connection connection, Object object)throws SQLException;
+
+    void createTable(Connection connection)throws SQLException;
+
+    private void add(Connection connection, Object str) throws SQLException {
+        //...........
+    }
+
+    private void addDefault(Connection connection) throws SQLException {
+        // .........
+    }
+
+    int  size()throws SQLException;
+
+}
