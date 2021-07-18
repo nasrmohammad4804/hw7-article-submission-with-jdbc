d8761ea8251d6f8b60640d4b497af38d48532c98
mohammad nasr <nasrmohammad4804@gmail.com>
2021-07-18 16:32:04 +0430

    create interface for show all and adding to user and tag and default for user and category size for all

---
+++ Hw7/src/BaseRepository.java
@@ -0,0 +1,21 @@
+import java.sql.Connection;
+import java.util.List;
+
+public interface BaseRepository {
+
+
+    void showAll(Connection connection, Object object);
+
+    void createTable(Connection connection);
+
+    private void add(Connection connection, Object str) {
+        //...........
+    }
+
+    private void addDefault(Connection connection) {
+        // .........
+    }
+
+    int  size();
+
+}
