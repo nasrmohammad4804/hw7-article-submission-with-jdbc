2a5074e2d497492334e5b9c414ec1d73e9dfd536
mohammad nasr <nasrmohammad4804@gmail.com>
2021-07-18 19:09:14 +0430

    add method showAll for show all tag in database

--- Hw7/src/repository/TagRepository.java
+++ Hw7/src/repository/TagRepository.java
@@ -6,6 +6,24 @@ public class TagRepository implements BaseRepository{
     @Override
     public void showAll(Connection connection, Object object) throws SQLException {
 
+        int category_id=(int) object;
+
+        PreparedStatement preparedStatement=connection.prepareStatement("" +
+                "select temp from (select t.title as temp from tag as t inner join temp_article_tag as tat on tat.tag_id=t.id inner  join article as a "+
+                "on a.id=tat.article_id where a.category_id = ? ) as ptr group by temp ");
+
+        System.out.println("categoryId : "+category_id);
+
+        preparedStatement.setInt(1,category_id);
+        ResultSet resultSet =preparedStatement.executeQuery();
+
+        while (resultSet.next()){
+            System.out.println(resultSet.getString("temp"));
+        }
+
+        preparedStatement.close();
+
+        System.out.println("------------------------------------------------------------");
     }
 
     @Override
